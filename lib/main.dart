01bd2c5dad3a461235f2fa54737bd88d5d040322
Md. Abir Hasan <abirhasanshuvo789@gmail.com>
2025-03-23 10:30:50 +0600

    better player ready for used in eduTV

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:better_player_plus/better_player_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:restapi_testing/aamar_pay/player/player.dart';
-
-
+import 'package:restapi_testing/video_player/video_player.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,6 +14,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        debugShowCheckedModeBanner: false, home: YoutubePlayerScreen());
+        debugShowCheckedModeBanner: false, home: VideoPlayerScreen());
   }
 }
